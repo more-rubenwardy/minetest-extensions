f4ab26b6b77c16929b93d6d6c7360adc3c76479d
Rubenwardy <anjayward@gmail.com>
2012-08-22 19:06:09 +0100

    Script and Readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-
+ Rubenwardy's Mod Forum
+ ======================
+ 
+ A php and mysgl forum for mods
+ 
+ 
+ Database
+ ========
+ 
+ USERS
+ -Name
+ -Email
+ -Password
+ -Level
+ -Stars
+ -Avatar
+ -Location
--- scripts/setup.php
+++ scripts/setup.php
@@ -1,34 +1,28 @@
 <?php
-checklogin("rubenwardy","multas");
+$session_start();
 
-function checklogin($user,$pass){
-	if (!file_exists("../data/users/$user/user.pass"))
-		echo "User does not exist";
-
-	
-}
-?>
-
-<!--php
 $handle = mysql_pconnect("mysql.serversfree.com","u372522788_admin","password");
 
 if (!$handle)
 die("MySQL - Error connecting to the MySQL database");
 
-mysql_select_db("u372522788_minetest",$handle);
+mysql_select_db("u372522788_minetest",$handle) or die("Error Switching DB");
 
-checklogin("rubenwardy","multas");
-
-function checklogin($user,$pass){
-	$user= mysql_escape_string ( $user );
-	$res = mysql_query("SELECT * FROM users WHERE name='".$user."'",$handle) or die("ERROR");
-	$row = mysql_fetch_row($res);
-	echo $row[3];
-
+function checklogin($user,$pass,$handle){
+	$us= mysql_escape_string ($user);
+	$res = mysql_query("SELECT * FROM users WHERE name='$us'",$handle) or die("query error");
+	$row = mysql_fetch_row($res) or die("row error");
+	
 	if ($row[3]==$pass){
 		return 1;
 	}else{
 		return 0;
 	}
 }
--->
+
+function getUser($user){
+	$res = mysql_query("SELECT * FROM users WHERE name='$us'",$handle) or die("query error");
+	$row = mysql_fetch_row($res) or die("row error");
+	return $row;
+}
+?>
