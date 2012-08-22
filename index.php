d0626009e11526c6fe76dd9ee94ba27aca7ba316
Rubenwardy <anjayward@gmail.com>
2012-08-22 16:38:40 +0100

    Initial Push

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en-gb">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-2">
+<meta name="Generator" content="Serif WebPlus X5 (13.0.3.029)">
+<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE8">
+<title>Home - Multa</title>
+<meta name="keywords" content="">
+<meta name="author" content="Andrew Ward">
+<meta name="copyright" content="Copyright &#xA9; to Andrew Ward">
+<meta http-equiv="Content-Language" content="en-gb">
+<meta name="robots" content="index,follow">
+<!--<link rel="icon" href="favicon.ico" type="image/x-icon">-->
+
+
+<body text="#000000" style="background-color:#ffffff; text-align:center; height:610px;">
+<div style="background-color:transparent;text-align:left;margin-left:auto;margin-right:auto;position:relative;width:900px;height:1000px;">
+<!--The Header-->
+<img src="header.png" width="900" height="100" border="0" alt="" style="position:absolute;left:0px;top:0px;">
+
+
+<!--The footer-->
+
+
+
+<!--The content-->
+
+</div>
+</body>
+</html>
