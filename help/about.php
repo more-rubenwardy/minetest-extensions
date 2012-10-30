13b4cdfad0b28ed81041205b82cdb76b4c5ef4fe
Rubenwardy <anjayward@gmail.com>
2012-10-30 20:28:18 +0000

    About.php

--- help/about.php
+++ help/about.php
@@ -1,16 +1,36 @@
 <?php
 include "../scripts/setup.php";
-$page_title="Index - Help";
+$page_title="About - Help";
 include "../scripts/pageheader.php";
 ?>
 
-<h1>Help</h1>
+<h1>About this Project</h1>
 
+<h2>Developer</h2>
+This game mod/extension browser/forum was created completely in code by Andrew "rubenwardy" Ward.
+
+<h2>Why a new one?</h2>
+The current mod forum is, well unorganized and not very well managed.<br />
+That is why, I am making a new one, for c55 to possibly implement <br />
+(if not, it will still be used by the three mod-manager mods/scripts).<br />
+
+<h2>The Aims</h2>
 <ul>
-<li><a href="entries.php">Adding an Entry</a></li>
-<li><a href="markup.php">Description Markup</a></li>
-<li><a href="tags.php">Entry Tags</a></li>
+<li>An easier way to find mods</li>
+<li>A like/dislike system means players can find the best mods for them</li>
+<li>A database for the Mod-Manager Mods</li>
+<li>No more: minetest is soo boring, there is no mobs, or cart... :(</li>
+<li>No more: how do i install mods?</li>
 </ul>
+
+<h2>It features</h2>
+<ul>
+<li>Categories and Tag Searching</li>
+<li>Like</li>
+<li>Mod entry adding and editing</li>
+</ul>
+
+
 
 <?php
 include "../scripts/pagefooter.php";
